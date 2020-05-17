5c4d0c66e1f0dcb263a3cc862238192ca11e5b19
yutz9836 <40331846+yutz9836@users.noreply.github.com>
2020-05-18 00:04:50 +0800

    -to_

--- lineBotGit/production_settings.py
+++ lineBotGit/production_settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 from .settings import *
 
-import dj-database-url
+import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(),
